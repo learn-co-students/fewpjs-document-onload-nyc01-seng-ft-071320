59bddcdd5c6a41f9d6db43c292b95b09efc66fb1
amitdesh <amitdesh2014@gmail.com>
2020-08-24 18:19:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     updateDom()
+// });
+
+
+// function updateDom() { 
+//     el = document.getElementById("text");
+//     el.innerHTML = "This is really cool!";
+// }
+
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
