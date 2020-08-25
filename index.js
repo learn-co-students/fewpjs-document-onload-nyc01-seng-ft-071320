4cc3ef52237b4ff7bc99a354ee07dde41606ee0b
DBenson1017 <devinbenson@gmail.com>
2020-08-24 20:13:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let message = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("The DOM has loaded");
+    message.textContent ='This is really cool!';
+});
+
+// function e(){
+//     let message = document.getElementById('text');
+//     message.textContent ='This is really cool!';
+// };
+
+
